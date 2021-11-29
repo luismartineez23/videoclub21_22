f1124bd0d6cdb4d9777e34d6cec497aedffe4bf4
Luis Martínez <alumno@vm1>
2021-11-29 08:43:32 +0100

    Solución al ejercicio 3

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -16,27 +16,27 @@
 
              <div class="form-group">
                 <label for="title">Título</label>
-                <input type="text" name="title" id="title" value="{{$pelicula['title']}}" class="form-control">
+                <input type="text" name="title" id="title" value="{{$pelicula->title}}" class="form-control">
              </div>
 
              <div class="form-group">
                 <label for="title">A&ntilde;o</label>
-               <input type="number" name="year" id="year" value="{{$pelicula['year']}}">
+               <input type="number" name="year" id="year" value="{{$pelicula->year}}">
             </div>
 
             <div class="form-group">
                 <label for="title">Director</label>
-               <input type="text" name="director" id="director" value="{{$pelicula['director']}}" class="form-control">
+               <input type="text" name="director" id="director" value="{{$pelicula->director}}" class="form-control">
             </div>
 
             <div class="form-group">
                 <label for="title">P&oacute;ster</label>
-               <input type="text" name="poster" id="poster" value="{{$pelicula['poster']}}" class="form-control">
+               <input type="text" name="poster" id="poster" value="{{$pelicula->poster}}" class="form-control">
             </div>
 
              <div class="form-group">
                 <label for="synopsis">Resumen</label>
-                <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula['synopsis']}}</textarea>
+                <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula->synopsis}}</textarea>
              </div>
 
              <div class="form-group text-center">
